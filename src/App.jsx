67fb3e1e0da3b98c188c58b42b1f335b4135a469
Satish MUdbhari <satish@Roshans-MacBook-Pro.local>
2025-08-19 17:44:36 +0100

    fix issue

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import './css/index.css'
+import NoahPortfolio from './components/home'
+
+function App() {
+
+
+  return (
+    <>
+      <NoahPortfolio/>
+    </>
+  )
+}
+
+export default App
